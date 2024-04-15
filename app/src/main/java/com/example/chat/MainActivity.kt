b7591f1102609e024db196a8b27f708358a7d4e6
lenovo <3108531642@qq.com>
2024-04-15 10:42:18 +0800

    初始化UI

---
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.chat
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.chat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private  val SPEECH_REQUEST_CODE = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.sayingBtn.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+
+            val packageManager:PackageManager = packageManager
+            val activities:List<ResolveInfo> = packageManager.queryIntentActivities(intent,0)
+            if (activities.isNotEmpty()){
+                startActivityForResult(intent,SPEECH_REQUEST_CODE)
+            }
+
+        }
+    }
+}
